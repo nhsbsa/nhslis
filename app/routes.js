bd45f37ee2a9a88db46bfb739ef9a1a036e979de
Jon Greer <jongreer@Jons-MacBook-Pro.local>
2016-04-05 16:24:33 +0100

    added some logic to demo journeys

--- app/routes.js
+++ app/routes.js
@@ -71,16 +71,65 @@ module.exports = {
       res.redirect('/type-2-diabetes' + req.params[0]);
     });
 
-      
-      
+    
     // add your routes here
-
+    
     //LIS 0.1
+    var myWork;
+
     app.get('/lis/0', function (req, res) {
       res.render('lis/0/');
     });
+    
+    //partner
+    app.get('/lis/0/registration/partner-handler', function (req, res) {
+      console.log(req.query);
+      if (req.query.partner === 'yes') {
+        res.redirect('/lis/0/registration/partners-details');
+      } else {
+        res.redirect('/lis/0/registration/registration-summary');
+      }
+    });
+    
+    //about you summary
+    app.get('/lis/0/you/about-you-summary', function (req, res) {
+      res.render('lis/0/you/about-you-summary', {
+        'myWork' : myWork
+      });
+    });
+    
+    //education
+    app.get('/lis/0/you/education-handler', function(req, res) {
+      console.log(req.query);
+      if (req.query.education === 'yes') {
+        res.redirect('/lis/0/you/course');
+      } else {
+        res.redirect('/lis/0/you/work');
+      }
+    });
 
+    //work
+    app.get('/lis/0/you/work-handler', function(req, res) {
+      console.log(req.query);
+      if (req.query.work === 'yes') {
+        myWork = 'Yes';
+        res.redirect('/lis/0/you/work-type');
+      } else {
+        myWork = 'No';
+        res.redirect('/lis/0/you/benefits/pension');
+      }
+    });
 
+    //pension
+    app.get('/lis/0/you/benefits/pension-handler', function(req, res) {
+      console.log(req.query);
+      if (req.query.pension === 'no') {
+        res.redirect('/lis/0/you/benefits/benefit-group1');
+      } else {
+        res.redirect('/lis/0/you/benefits/pension-type');
+      }
+    });
+    
       
       
     // Change or cancel appointment fork:
